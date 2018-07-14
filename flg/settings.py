bafe7f81f9a1c5f954f1cd0b31f62e884e6e7da9
MIYATA Tadaaki <andes@flg.jp>
2018-07-14 13:53:46 +0900

    test for openshift

--- flg/settings.py
+++ flg/settings.py
@@ -127,7 +127,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = '/flg/srv/http/django/apps/static'
+STATIC_ROOT = '/flg/srv/http/django/'
 STATIC_URL = '/apps/static/'
-
 
